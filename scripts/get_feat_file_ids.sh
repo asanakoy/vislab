03a9bd9b04b15f9ed01ffb62a6d65a7564c1dedf
Artsiom Sanakoyeu <artsiom.sanakoyeu@iwr.uni-heidelberg.de>
2016-07-07 13:57:57 +0200

    gzcat -> zcat

--- scripts/get_feat_file_ids.sh
+++ scripts/get_feat_file_ids.sh
@@ -1,2 +1,3 @@
 FEAT_DIR=$HOME/work/vislab-git/data/feats
-find $FEAT_DIR -name "*.txt.gz" -exec sh -c "echo {} && gzcat {} | cut -d ' ' -f 2 | cut -c 3- > {}.ids.txt" \;
+GZCAT_COMMAND='zcat'
+find $FEAT_DIR -name "*.txt.gz" -exec sh -c "echo {} && ${GZCAT_COMMAND} {} | cut -d ' ' -f 2 | cut -c 3- > {}.ids.txt" \;
