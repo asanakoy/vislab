7d3b4d523d450cc0b68fb40dd381261eff9ab3c9
Artsiom Sanakoyeu <artsiom.sanakoyeu@iwr.uni-heidelberg.de>
2016-07-18 21:17:41 +0200

    config pathes appended

--- vislab/__init__.py
+++ vislab/__init__.py
@@ -8,5 +8,11 @@ with open(repo_dirname + '/config.json', 'r') as f:
 for path_name, path in config['paths'].iteritems():
     config['paths'][path_name] = os.path.expanduser(path)
 
+# make peredict_temp dir process specific
+config['paths']['predict_temp'] += '_{}'.format(os.getpid())
+
+import sys
+sys.path.append(os.path.join(config['paths']['caffe'], 'python'))
+
 import util
 # import feature
