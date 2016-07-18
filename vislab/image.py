49c98d6e5e6bdd842a12a3c34a5480d272d3c0f0
Artsiom Sanakoyeu <artsiom.sanakoyeu@iwr.uni-heidelberg.de>
2016-07-18 21:16:43 +0200

    upd

--- vislab/image.py
+++ vislab/image.py
@@ -7,3 +7,13 @@ def get_image_for_filename(image_filename):
         return image
     else:
         return None
+
+
+def image2jpg(src_path, dst_parh):
+    import subprocess
+    import shlex
+    cmd = 'convert {0} {0}'.format(src_path, dst_parh)
+    print cmd
+    p = subprocess.Popen(
+        shlex.split(cmd))
+    p.wait()
